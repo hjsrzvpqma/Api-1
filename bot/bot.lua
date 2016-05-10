0018ab9ff4de0679c235d5209d4d783e7bb26ced
Mohammad Mahdi <MohammadArak@users.noreply.github.com>
2016-05-11 00:48:28 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -235,7 +235,7 @@ function create_config()
     globally_banned = {},
     mkgroup = {founded = '', founder = '', title = '', gtype = '', uid = ''},
     realm = {},
-    sudo_users = {179071599}
+    sudo_users = {[179071599] = 179071599}
   }
   save_config()
 end
